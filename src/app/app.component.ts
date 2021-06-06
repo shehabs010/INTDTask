69edac1294af82776db74529123bfccaa2cbb56a
shehabs010 <shehabs010@gmail.com>
2021-06-06 10:05:43 +0200

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,138 +6,150 @@ import { Todo } from './models/todo.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-
-
 export class AppComponent implements OnInit {
-
   title = 'todo-list';
-  todos:Array <Todo> = window.localStorage.getItem('todos-List') ? JSON.parse(window.localStorage.getItem('todos-List') || '[]') : [];
-  completedTodoList:Array<Todo> = window.localStorage.getItem('todos-Done-List') ? JSON.parse(window.localStorage.getItem('todos-Done-List') || '[]') : [];
+  todos: Array<Todo> = window.localStorage.getItem('todos-List')
+    ? JSON.parse(window.localStorage.getItem('todos-List') || '[]')
+    : [];
+  completedTodoList: Array<Todo> = window.localStorage.getItem(
+    'todos-Done-List'
+  )
+    ? JSON.parse(window.localStorage.getItem('todos-Done-List') || '[]')
+    : [];
   editObject = null;
-  endTaskTime= 0;
- 
-  handelEvent(data:any){
-      if(!data.id){
-        data.id = Math.floor(Math.random() * 1000)
-        this.todos.push(data)
+  endTaskTime = 0;
+  isDisabled=false;
+
+  handelEvent(data: any) {
+    if (!data.id) {
+      data.id = Math.floor(Math.random() * 1000);
+      this.todos.push(data);
+      window.localStorage.setItem('todos-List', JSON.stringify(this.todos));
+      this.endTimeNotification(data);
+    } else {
+      let checkEditItem = this.todos.find((x) => x.id == data.id);
+      if (checkEditItem != null) {
+        clearTimeout(checkEditItem.notification);
+        let indexOfItem = this.todos.indexOf(data);
+        this.todos.splice(indexOfItem, 1);
+        this.isDisabled =false;
+        this.todos.push(data);
         window.localStorage.setItem('todos-List', JSON.stringify(this.todos));
         this.endTimeNotification(data);
-      }else{
-        let checkEditItem = this.todos.find(x =>x.id == data.id);
-        if(checkEditItem != null)
-        {
-          clearTimeout(checkEditItem.notification);
-          let indexOfItem=this.todos.indexOf(data)
-           this.todos.splice(indexOfItem , 1);
-          this.todos.push(data)
-          window.localStorage.setItem('todos-List', JSON.stringify(this.todos));
-          this.endTimeNotification(data);
-        }
       }
-   
+    }
   }
-  handelEventComplete(serverData:any){
-    let todo = this.todos.filter(x=> x.id == serverData)[0];
-    todo.status= true;
+  handelEventComplete(serverData: any) {
+    let todo = this.todos.filter((x) => x.id == serverData)[0];
+    todo.status = true;
     this.completedTodoList.push(todo);
-    window.localStorage.setItem('todos-Done-List', JSON.stringify(this.completedTodoList));
+    window.localStorage.setItem(
+      'todos-Done-List',
+      JSON.stringify(this.completedTodoList)
+    );
     let x = this.todos.indexOf(todo);
-    this.todos.splice(x,1)
+    this.todos.splice(x, 1);
     window.localStorage.setItem('todos-List', JSON.stringify(this.todos));
-    
   }
-  handelEventNotComplete(serverDatas:any){
-    let unTodoItem = this.completedTodoList.filter(x=> x.id == serverDatas)[0];
-    unTodoItem.status= false;
+  handelEventNotComplete(serverDatas: any) {
+    let unTodoItem = this.completedTodoList.filter(
+      (x) => x.id == serverDatas
+    )[0];
+    unTodoItem.status = false;
     let itemIndex = this.completedTodoList.indexOf(unTodoItem);
-    this.completedTodoList.splice(itemIndex,1)
-    window.localStorage.setItem('todos-Done-List', JSON.stringify(this.completedTodoList));
+    this.completedTodoList.splice(itemIndex, 1);
+    window.localStorage.setItem(
+      'todos-Done-List',
+      JSON.stringify(this.completedTodoList)
+    );
     this.todos.push(unTodoItem);
     window.localStorage.setItem('todos-List', JSON.stringify(this.todos));
   }
-  handelEventEdit(serverEditData: any){
+  handelEventEdit(serverEditData: any) {
     this.editObject = serverEditData;
+    this.isDisabled = true;
   }
-  handelEventDelete(serverDeleteData: any){
-    if(serverDeleteData.status){
-      let todoItem = this.completedTodoList.filter(x => x.id === serverDeleteData)[0];
+  handelEventDelete(serverDeleteData: any) {
+    if (serverDeleteData.status) {
+      let todoItem = this.completedTodoList.filter(
+        (x) => x.id === serverDeleteData
+      )[0];
       let itemIndex = this.completedTodoList.indexOf(todoItem);
-      this.completedTodoList.splice(itemIndex,1)
-      window.localStorage.setItem('todos-Done-List', JSON.stringify(this.completedTodoList));
-    }else{
-      let todoItem = this.todos.filter(x => x.id === serverDeleteData)[0];
+      this.completedTodoList.splice(itemIndex, 1);
+      window.localStorage.setItem(
+        'todos-Done-List',
+        JSON.stringify(this.completedTodoList)
+      );
+    } else {
+      let todoItem = this.todos.filter((x) => x.id === serverDeleteData)[0];
       let itemIndex = this.todos.indexOf(todoItem);
-      this.todos.splice(itemIndex,1)
+      this.todos.splice(itemIndex, 1);
       window.localStorage.setItem('todos-List', JSON.stringify(this.todos));
     }
+  }
+  endTimeNotification(taskNotification: Todo) {
+    var m1 = moment(new Date(taskNotification.dueDate));
+    var m2 = moment(new Date());
+    var m3 = m1.diff(m2, 'minutes') * 60;
 
-  }
-  endTimeNotification(taskNotification:Todo){
-    var m1 = moment(new Date(taskNotification.dueDate)); 
-var m2 = moment(new Date());
-var m3 = m1.diff(m2,'minutes') * 60;
-
-      // let toDay =new Date().getTime();
-      // let dueDateTask = new Date(taskNotification.dueDate).get();
-      // let time = dueDateTask - toDay;
-     console.log(m3)
-     if(m3 > 0){
-       taskNotification.notification = setTimeout(() => {
-         this.showNotifcation(taskNotification)   
-        },m3 * 1000);
-      }
-     }
- 
- 
-  sortByTasks(){
-     let sortedTodos = _.sortBy(this.todos, [function(o) { return o.dueDate}]);
-     return sortedTodos
-  }
-  sortByDoneTasks(){
-    let sortedDoneTodos = _.sortBy(this.completedTodoList, [function(o) { return o.dueDate}]);
-    return sortedDoneTodos
- }
-  showNotifcation(endTaskTime: any){
-    const notification = new Notification(endTaskTime.dueDate,{
-      body:endTaskTime.text
-    });
-    
-  }
-  ngOnInit(): void {
-    
-  //Let's check if the browser supports notifications
-  if (!("Notification" in window)) {
-    alert("This browser does not support desktop notification");
-  }
-
-  // Let's check whether notification permissions have already been granted
-  else if (Notification.permission === "granted") {
-    // If it's okay let's create a notification
-    for(let i= 0; i <this.todos.length; i++){
-      let taskNotification = this.todos[i];
-      this.endTimeNotification(taskNotification)
+    console.log(m3);
+    if (m3 > 0) {
+      taskNotification.notification = setTimeout(() => {
+        this.showNotifcation(taskNotification);
+      }, m3 * 1000);
     }
   }
 
-  // Otherwise, we need to ask the user for permission
-  else if (Notification.permission !== "denied") {
-
-    Notification.requestPermission().then( (permission) => {
-
-      // If the user accepts, let's create a notification
-      if (permission === "granted") {
-        for(let i= 0; i <this.todos.length; i++){
-          let taskNotification = this.todos[i];
-          this.endTimeNotification(taskNotification)
-        }
-
-      }
+  sortByTasks() {
+    let sortedTodos = _.sortBy(this.todos, [
+      function (o) {
+        return o.dueDate;
+      },
+    ]);
+    return sortedTodos;
+  }
+  sortByDoneTasks() {
+    let sortedDoneTodos = _.sortBy(this.completedTodoList, [
+      function (o) {
+        return o.dueDate;
+      },
+    ]);
+    return sortedDoneTodos;
+  }
+  showNotifcation(endTaskTime: any) {
+    const notification = new Notification(endTaskTime.dueDate, {
+      body: endTaskTime.text,
     });
   }
-  // At last, if the user has denied notifications, and you
-  // want to be respectful there is no need to bother them any more.
-  }
+  ngOnInit(): void {
+    //Let's check if the browser supports notifications
+    if (!('Notification' in window)) {
+      alert('This browser does not support desktop notification');
+    }
 
+    // Let's check whether notification permissions have already been granted
+    else if (Notification.permission === 'granted') {
+      // If it's okay let's create a notification
+      for (let i = 0; i < this.todos.length; i++) {
+        let taskNotification = this.todos[i];
+        this.endTimeNotification(taskNotification);
+      }
+    }
+
+    // Otherwise, we need to ask the user for permission
+    else if (Notification.permission !== 'denied') {
+      Notification.requestPermission().then((permission) => {
+        // If the user accepts, let's create a notification
+        if (permission === 'granted') {
+          for (let i = 0; i < this.todos.length; i++) {
+            let taskNotification = this.todos[i];
+            this.endTimeNotification(taskNotification);
+          }
+        }
+      });
+    }
+
+  }
 }
